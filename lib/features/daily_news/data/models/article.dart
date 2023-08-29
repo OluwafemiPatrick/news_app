2a1c467deda48e6916ac991befd25f6f740572b9
Olarewaju Oluwafemi <oopatrick@Olarewajus-MacBook-Pro.local>
2023-08-29 15:35:47 +0100

    completed my flutter clean architecture project

--- lib/features/daily_news/data/models/article.dart
+++ lib/features/daily_news/data/models/article.dart
@@ -3,6 +3,7 @@ import 'package:news_app_clean_architecture/features/daily_news/domain/entities/
 import '../../../../core/constants/constants.dart';
 
 @Entity(tableName: 'article',primaryKeys: ['id'])
+
 class ArticleModel extends ArticleEntity {
   const ArticleModel({
     int ? id,
